2f12b819c6496dd2afd21525f37dc5abe0a00bcd
Akash Thawait <akash.thawait@litmusworld.com>
2017-10-09 21:26:57 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ import 'rxjs/add/operator/map';
 })
 export class AppComponent {
   gyms = [];
-  title: string = 'My first AGM project';
   lat: number = 51.678418;
   lng: number = 7.809007;
   zoom = 18;
